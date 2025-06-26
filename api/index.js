a72f4c1e2ade03e493b03969e52ed59d382c31cc
Marcos <mjmarco74@gmail.com>
2025-06-26 16:22:52 -0600

    cors v2

--- api/index.js
+++ api/index.js
@@ -10,7 +10,10 @@ const teacher = require("../src/routes/teacher.routes")
 
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 // Ruta base para utilizar el servicio
 app.get("/api", function(req, res) {
