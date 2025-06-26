582c5a7767fd5adae62a6130a9f0f2a07a3c7994
Marcos <mjmarco74@gmail.com>
2025-06-26 16:15:35 -0600

    cors corregido

--- api/index.js
+++ api/index.js
@@ -10,16 +10,7 @@ const teacher = require("../src/routes/teacher.routes")
 
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: false,
-}));
+app.use(cors());
 
 // Ruta base para utilizar el servicio
 app.get("/api", function(req, res) {
